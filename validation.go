bd914516cbc4ac60464793bc5404fcab6bdc9a8e
Cristian Kubis <me.tsurai@gmail.com>
2013-12-19 12:31:40 +0100

    more validator

--- validation.go
+++ validation.go
@@ -1,6 +1,7 @@
 package amber
 
 import (
+	"fmt"
 	"time"
 	"reflect"
 )
@@ -16,8 +17,11 @@ type ValidationResult struct {
 
 func (v *Validation) addValidationResult(valid bool, message string) *ValidationResult {
 		result := &ValidationResult{valid, message}
-		v.results = append(v.results, result)
 
+		if result != nil {
+			v.results = append(v.results, result)
+		}
+		
 		return result
 }
 
@@ -51,6 +55,52 @@ func (v *Validation) Required(obj interface{}) *ValidationResult {
 	val := reflect.ValueOf(obj)
 	if val.Kind() == reflect.Slice {
 		return v.addValidationResult(val.Len() > 0, defaultMessage)
+	}
+
+	return nil
+}
+
+func (v *Validation) Min(obj interface{}, min int) *ValidationResult {
+	defaultMessage := fmt.Sprintf("Small than %d", min)
+
+	if obj == nil {
+		return v.addValidationResult(false, defaultMessage)
+	}
+
+	if val, ok := obj.(int); ok {
+		return v.addValidationResult(val >= min, defaultMessage)
+	}
+
+	if val, ok := obj.(string); ok {
+		return v.addValidationResult(len(val) >= min, defaultMessage)
+	}
+
+	val := reflect.ValueOf(obj)
+	if val.Kind() == reflect.Slice {
+		return v.addValidationResult(val.Len() >= min, defaultMessage)
+	}
+
+	return nil
+}
+
+func (v *Validation) Max(obj interface{}, max int) *ValidationResult {
+	defaultMessage := fmt.Sprintf("Larger than %d", max)
+
+	if obj == nil {
+		return v.addValidationResult(false, defaultMessage)
+	}
+
+	if val, ok := obj.(int); ok {
+		return v.addValidationResult(val <= max, defaultMessage)
+	}
+
+	if val, ok := obj.(string); ok {
+		return v.addValidationResult(len(val) <= max, defaultMessage)
+	}
+
+	val := reflect.ValueOf(obj)
+	if val.Kind() == reflect.Slice {
+		return v.addValidationResult(val.Len() <= max, defaultMessage)
 	}
 
 	return nil
